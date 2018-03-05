cd7426442e6a11b571ba7d57e4587e7332d8e3ca
ironpa <ireneusz.parysz@gmail.com>
2018-03-05 15:20:35 +0100

    UI upgrade

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Ireneusz Parysz on 05.03.2018.
+//  Copyright © 2018 Ireneusz Parysz. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
