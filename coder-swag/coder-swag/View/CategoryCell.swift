75cfaa64e994439efdd6386b39bf14b7ac944652
ironpa <ireneusz.parysz@gmail.com>
2018-03-05 15:21:08 +0100

    assets added

--- coder-swag/coder-swag/View/CategoryCell.swift
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class CategoryCell: UITableViewCell {
 
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
